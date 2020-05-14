40ef7abfa86c7bac57501c798c5f8e1f5287f3de
dreamisadream <dreamisadream@gmail.com>
2020-05-14 18:29:23 +0800

    ld

---
+++ x86/ld/simple.c
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Description                                                                             //
+// 		This is the very basic example to show how to link elf file by yourself.           //
+//      But actually, we link nothing :p                                                   //
+//                                                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Usage                                                                                   //
+// 		gcc -c -fno-builtin -masm=intel simple.c -m32                                      //
+// 		ld -static -e entry -o simple simple.o -m elf_i386                                 //
+//      ./simple                                                                           //
+//      echo $?     <- should be 88                                                        //
+//                                                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+char *msg = "hi\n";
+
+void say_hi()
+{
+	// write(stdout, str, 3)
+	asm(
+		"mov ebx, 1;"
+		"mov ecx, %0;"
+		"mov edx, 3;"
+		"mov eax, 4;"
+		"int 0x80;"
+		::"r"(msg):"eax", "ebx", "ecx", "edx"
+	);
+}
+
+void exit()
+{
+	// exit(88)
+	asm(
+		"mov ebx, 88;"
+		"mov eax, 1;"
+		"int 0x80;"
+		);
+}
+
+void entry()
+{
+	say_hi();
+	exit();
+}
---
+++ x86_64/ld/simple.c
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Description                                                                             //
+// 		This is the very basic example to show how to link elf file by yourself.           //
+//      But actually, we link nothing :p                                                   //
+//                                                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Usage                                                                                   //
+// 		gcc -c -fno-builtin -masm=intel simple.c                                           //
+// 		ld -static -e entry -o simple simple.o                                             //
+//      ./simple                                                                           //
+//      echo $?     <- should be 88                                                        //
+//                                                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+char *msg = "hi\n";
+
+void say_hi()
+{
+	// write(stdout, str, 3)
+	asm(
+		"mov rdi, 1;"
+		"mov rsi, %0;"
+		"mov rdx, 3;"
+		"mov rax, 1;"
+		"syscall;"
+		::"r"(msg):"rdi", "rsi", "rdx", "rax"
+	);
+}
+
+void exit()
+{
+	// exit(88)
+	asm(
+		"mov rdi, 88;"
+		"mov rax, 0x3c;"
+		"syscall;"
+		);
+}
+
+void entry()
+{
+	say_hi();
+	exit();
+}
